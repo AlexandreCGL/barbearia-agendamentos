81d3c94794d44ea5d43b004403e2c8eb2e85a7d9
alexandrecardoso590@gmail.com <alexandrecardoso590@gmail.com>
2026-06-16 01:04:16 -0300

    Projeto Barbearia: configuracao do PHPUnit e primeiros testes

---
+++ tests/AgendamentoTest.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+class AgendamentoTest extends TestCase
+{
+    public function testHorarioDisponivelRetornaVerdadeiro(): void
+    {
+        $this->assertTrue(true);
+    }
+}
